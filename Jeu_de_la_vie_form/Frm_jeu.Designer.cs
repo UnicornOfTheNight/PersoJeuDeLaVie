412069a355eb8da00eb1dfb65526d2494f42f8c7
noelm <maeva.noel99@outlook.fr>
2024-02-28 20:38:29 +0100

    first commit

---
+++ Jeu_de_la_vie_form/Frm_jeu.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Jeu_de_la_vie_form
+{
+	partial class Frm_jeu
+	{
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		/// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur Windows Form
+
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.DgvJeu = new System.Windows.Forms.DataGridView();
+			this.btn_Random = new System.Windows.Forms.Button();
+			this.btn_Suivant = new System.Windows.Forms.Button();
+			this.Num_Hauteur = new System.Windows.Forms.NumericUpDown();
+			this.Num_Largeur = new System.Windows.Forms.NumericUpDown();
+			this.Lbl_Hauteur = new System.Windows.Forms.Label();
+			this.Lbl_Largeur = new System.Windows.Forms.Label();
+			this.Btn_Taille = new System.Windows.Forms.Button();
+			this.lbl_Erreur_Hauteur = new System.Windows.Forms.Label();
+			this.lbl_Erreur_Largeur = new System.Windows.Forms.Label();
+			this.btn_Play = new System.Windows.Forms.Button();
+			this.btn_Pause = new System.Windows.Forms.Button();
+			this.btn_Precedent = new System.Windows.Forms.Button();
+			this.lbl_Generation = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.DgvJeu)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.Num_Hauteur)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.Num_Largeur)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// DgvJeu
+			// 
+			this.DgvJeu.AllowUserToAddRows = false;
+			this.DgvJeu.AllowUserToDeleteRows = false;
+			this.DgvJeu.AllowUserToOrderColumns = true;
+			this.DgvJeu.AllowUserToResizeColumns = false;
+			this.DgvJeu.AllowUserToResizeRows = false;
+			this.DgvJeu.BackgroundColor = System.Drawing.Color.White;
+			this.DgvJeu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.DgvJeu.ColumnHeadersVisible = false;
+			this.DgvJeu.Enabled = false;
+			this.DgvJeu.GridColor = System.Drawing.Color.Black;
+			this.DgvJeu.Location = new System.Drawing.Point(47, 145);
+			this.DgvJeu.Name = "DgvJeu";
+			this.DgvJeu.ReadOnly = true;
+			this.DgvJeu.RowHeadersVisible = false;
+			this.DgvJeu.RowHeadersWidth = 51;
+			this.DgvJeu.RowTemplate.Height = 24;
+			this.DgvJeu.Size = new System.Drawing.Size(796, 590);
+			this.DgvJeu.TabIndex = 5;
+			// 
+			// btn_Random
+			// 
+			this.btn_Random.BackColor = System.Drawing.Color.LightBlue;
+			this.btn_Random.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_Random.Location = new System.Drawing.Point(882, 58);
+			this.btn_Random.Name = "btn_Random";
+			this.btn_Random.Size = new System.Drawing.Size(115, 36);
+			this.btn_Random.TabIndex = 6;
+			this.btn_Random.Text = "Générer";
+			this.btn_Random.UseVisualStyleBackColor = false;
+			this.btn_Random.Click += new System.EventHandler(this.btn_Random_Click);
+			// 
+			// btn_Suivant
+			// 
+			this.btn_Suivant.BackColor = System.Drawing.Color.LightBlue;
+			this.btn_Suivant.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_Suivant.Location = new System.Drawing.Point(882, 179);
+			this.btn_Suivant.Name = "btn_Suivant";
+			this.btn_Suivant.Size = new System.Drawing.Size(115, 36);
+			this.btn_Suivant.TabIndex = 7;
+			this.btn_Suivant.Text = "Suivant";
+			this.btn_Suivant.UseVisualStyleBackColor = false;
+			this.btn_Suivant.Click += new System.EventHandler(this.btn_Suivant_Click);
+			// 
+			// Num_Hauteur
+			// 
+			this.Num_Hauteur.Font = new System.Drawing.Font("Cambria", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Num_Hauteur.Location = new System.Drawing.Point(16, 36);
+			this.Num_Hauteur.Name = "Num_Hauteur";
+			this.Num_Hauteur.Size = new System.Drawing.Size(62, 29);
+			this.Num_Hauteur.TabIndex = 10;
+			// 
+			// Num_Largeur
+			// 
+			this.Num_Largeur.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Num_Largeur.Location = new System.Drawing.Point(169, 37);
+			this.Num_Largeur.Name = "Num_Largeur";
+			this.Num_Largeur.Size = new System.Drawing.Size(55, 27);
+			this.Num_Largeur.TabIndex = 11;
+			// 
+			// Lbl_Hauteur
+			// 
+			this.Lbl_Hauteur.AutoSize = true;
+			this.Lbl_Hauteur.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Lbl_Hauteur.Location = new System.Drawing.Point(12, 9);
+			this.Lbl_Hauteur.Name = "Lbl_Hauteur";
+			this.Lbl_Hauteur.Size = new System.Drawing.Size(81, 23);
+			this.Lbl_Hauteur.TabIndex = 12;
+			this.Lbl_Hauteur.Text = "Hauteur";
+			// 
+			// Lbl_Largeur
+			// 
+			this.Lbl_Largeur.AutoSize = true;
+			this.Lbl_Largeur.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Lbl_Largeur.Location = new System.Drawing.Point(165, 9);
+			this.Lbl_Largeur.Name = "Lbl_Largeur";
+			this.Lbl_Largeur.Size = new System.Drawing.Size(78, 23);
+			this.Lbl_Largeur.TabIndex = 13;
+			this.Lbl_Largeur.Text = "Largeur";
+			// 
+			// Btn_Taille
+			// 
+			this.Btn_Taille.BackColor = System.Drawing.Color.LightBlue;
+			this.Btn_Taille.FlatAppearance.BorderSize = 0;
+			this.Btn_Taille.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Btn_Taille.Location = new System.Drawing.Point(88, 82);
+			this.Btn_Taille.Name = "Btn_Taille";
+			this.Btn_Taille.Size = new System.Drawing.Size(75, 36);
+			this.Btn_Taille.TabIndex = 14;
+			this.Btn_Taille.Text = "OK";
+			this.Btn_Taille.UseVisualStyleBackColor = false;
+			this.Btn_Taille.Click += new System.EventHandler(this.Btn_Taille_Click);
+			// 
+			// lbl_Erreur_Hauteur
+			// 
+			this.lbl_Erreur_Hauteur.AutoSize = true;
+			this.lbl_Erreur_Hauteur.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl_Erreur_Hauteur.Location = new System.Drawing.Point(285, 9);
+			this.lbl_Erreur_Hauteur.Name = "lbl_Erreur_Hauteur";
+			this.lbl_Erreur_Hauteur.Size = new System.Drawing.Size(308, 23);
+			this.lbl_Erreur_Hauteur.TabIndex = 15;
+			this.lbl_Erreur_Hauteur.Text = "La hauteur doit être entre 15 et 60.";
+			// 
+			// lbl_Erreur_Largeur
+			// 
+			this.lbl_Erreur_Largeur.AutoSize = true;
+			this.lbl_Erreur_Largeur.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl_Erreur_Largeur.Location = new System.Drawing.Point(285, 42);
+			this.lbl_Erreur_Largeur.Name = "lbl_Erreur_Largeur";
+			this.lbl_Erreur_Largeur.Size = new System.Drawing.Size(302, 23);
+			this.lbl_Erreur_Largeur.TabIndex = 16;
+			this.lbl_Erreur_Largeur.Text = "La largeur doit être entre 15 et 80.";
+			// 
+			// btn_Play
+			// 
+			this.btn_Play.BackColor = System.Drawing.Color.LightBlue;
+			this.btn_Play.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_Play.Location = new System.Drawing.Point(882, 100);
+			this.btn_Play.Name = "btn_Play";
+			this.btn_Play.Size = new System.Drawing.Size(115, 35);
+			this.btn_Play.TabIndex = 17;
+			this.btn_Play.Text = "Play";
+			this.btn_Play.UseVisualStyleBackColor = false;
+			this.btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
+			// 
+			// btn_Pause
+			// 
+			this.btn_Pause.BackColor = System.Drawing.Color.LightBlue;
+			this.btn_Pause.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_Pause.Location = new System.Drawing.Point(882, 141);
+			this.btn_Pause.Name = "btn_Pause";
+			this.btn_Pause.Size = new System.Drawing.Size(115, 32);
+			this.btn_Pause.TabIndex = 18;
+			this.btn_Pause.Text = "Pause";
+			this.btn_Pause.UseVisualStyleBackColor = false;
+			this.btn_Pause.Click += new System.EventHandler(this.btn_Pause_Click);
+			// 
+			// btn_Precedent
+			// 
+			this.btn_Precedent.BackColor = System.Drawing.Color.LightBlue;
+			this.btn_Precedent.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btn_Precedent.Location = new System.Drawing.Point(882, 221);
+			this.btn_Precedent.Name = "btn_Precedent";
+			this.btn_Precedent.Size = new System.Drawing.Size(115, 35);
+			this.btn_Precedent.TabIndex = 19;
+			this.btn_Precedent.Text = "Précedent";
+			this.btn_Precedent.UseVisualStyleBackColor = false;
+			this.btn_Precedent.Click += new System.EventHandler(this.btn_Precedent_Click);
+			// 
+			// lbl_Generation
+			// 
+			this.lbl_Generation.AutoSize = true;
+			this.lbl_Generation.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl_Generation.Location = new System.Drawing.Point(864, 335);
+			this.lbl_Generation.Name = "lbl_Generation";
+			this.lbl_Generation.Size = new System.Drawing.Size(144, 23);
+			this.lbl_Generation.TabIndex = 20;
+			this.lbl_Generation.Text = "Génération n° 0";
+			// 
+			// Frm_jeu
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.AliceBlue;
+			this.ClientSize = new System.Drawing.Size(1064, 790);
+			this.Controls.Add(this.lbl_Generation);
+			this.Controls.Add(this.btn_Precedent);
+			this.Controls.Add(this.btn_Pause);
+			this.Controls.Add(this.btn_Play);
+			this.Controls.Add(this.lbl_Erreur_Largeur);
+			this.Controls.Add(this.lbl_Erreur_Hauteur);
+			this.Controls.Add(this.Btn_Taille);
+			this.Controls.Add(this.Lbl_Largeur);
+			this.Controls.Add(this.Lbl_Hauteur);
+			this.Controls.Add(this.Num_Largeur);
+			this.Controls.Add(this.Num_Hauteur);
+			this.Controls.Add(this.btn_Suivant);
+			this.Controls.Add(this.btn_Random);
+			this.Controls.Add(this.DgvJeu);
+			this.MaximizeBox = false;
+			this.Name = "Frm_jeu";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Jeu de la vie";
+			((System.ComponentModel.ISupportInitialize)(this.DgvJeu)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.Num_Hauteur)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.Num_Largeur)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView DgvJeu;
+		private System.Windows.Forms.Button btn_Random;
+		private System.Windows.Forms.Button btn_Suivant;
+		private System.Windows.Forms.NumericUpDown Num_Hauteur;
+		private System.Windows.Forms.NumericUpDown Num_Largeur;
+		private System.Windows.Forms.Label Lbl_Hauteur;
+		private System.Windows.Forms.Label Lbl_Largeur;
+		private System.Windows.Forms.Button Btn_Taille;
+		private System.Windows.Forms.Label lbl_Erreur_Hauteur;
+		private System.Windows.Forms.Label lbl_Erreur_Largeur;
+		private System.Windows.Forms.Button btn_Play;
+		private System.Windows.Forms.Button btn_Pause;
+		private System.Windows.Forms.Button btn_Precedent;
+		private System.Windows.Forms.Label lbl_Generation;
+	}
+}
+
---
+++ Jeu_de_la_vie_form/Frm_jeu.cs
@@ -0,0 +1,447 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jeu_de_la_vie_form
+{
+	
+	public partial class Frm_jeu : Form
+	{
+		int nbGeneration = 0;
+		int hauteur = 50;
+		int largeur = 50;
+		int[,] tabIsAlive;
+		List<int[,]> lstIsAliveSauv = new List<int[,]>();
+		BackgroundWorker bw = new BackgroundWorker();
+		
+		/// <summary>
+		/// Initialisation des éléments
+		/// </summary>
+		public Frm_jeu()
+		{
+			InitializeComponent();
+
+			//Création des évènements
+			bw.DoWork +=
+				new DoWorkEventHandler(bw_DoWork);
+			bw.RunWorkerCompleted +=
+				new RunWorkerCompletedEventHandler(
+			bw_RunWorkerCompleted);
+			bw.ProgressChanged +=
+				 new ProgressChangedEventHandler(
+			bw_ProgressChanged);
+
+
+			Num_Largeur.Value = 60;
+			Num_Hauteur.Value = 60;
+			InitialisationDgv();
+
+			bw.WorkerSupportsCancellation = true;
+			bw.WorkerReportsProgress = true;
+			btn_Precedent.Enabled = false;
+		}
+
+		// This event handler updates the progress.
+		private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			//lbl_Generation.Text = "Génération n° " + nbGeneration;
+		}
+
+
+		/// <summary>
+		/// Excecution de la tache de fond
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void bw_DoWork(object sender, DoWorkEventArgs e)
+		{
+			MethodInvoker changeLbl = delegate
+			{
+				lbl_Generation.Text = "Génération n° " + nbGeneration;
+			};
+
+			//Création de la tâche de fond
+			BackgroundWorker worker = (BackgroundWorker)sender;
+			int[,] tabTmp = new int[largeur, hauteur];
+
+			//Initialisation du tableau tempon
+			for(int i = 0; i < largeur; i++)
+			{
+				for(int j = 0; j < hauteur; j++)
+				{
+					tabTmp[i, j] = -1;
+				} 
+			}
+
+			//Tant que le tableau tempon et le tableauIsAlive sont différents, pour savoir si tabIsAlive(n-1) = tabISAlive(n)
+			while (CompareTab(tabTmp) == false)
+			{
+				//Le tableau tempon prend la meme valeur que le tableauIsAlive
+				tabTmp = tabIsAlive;
+
+				//Si l'utilisateur a appuyé sur Pause on arrête la tache de fond
+				if (bw.CancellationPending == true)
+				{
+					e.Cancel = true;
+					return;
+				}
+				//Sinon on continue
+				else {		
+					//On passe à tabIsAlive(n+1)
+					NextStep();
+					Invoke(changeLbl);
+					//On attend 2 secondes avant de continuer
+					Thread.Sleep(2000);
+					
+				}
+			} 								
+		}
+
+		/// <summary>
+		/// Fonction pour comparer les valeurs du tableau IsAlive avec un autre tableau
+		/// </summary>
+		/// <param name="pTabTmp"></param>
+		/// <returns>Vrai si les 2 tableaux ont les mêmes valeurs, faux sinon</returns>
+		private bool CompareTab(int[,] pTabTmp)
+		{
+			bool isSame = true;
+
+			//On parcours la premiere dimension
+			for(int i = 1; i < largeur; i++)
+			{
+				//On parcours la deuxième dimension
+				for (int j = 1; j < hauteur; j++)
+				{
+					//Si les valeurs sont différentes la variable isSame prend la valeur fausse
+					if (tabIsAlive[i, j] != pTabTmp[i, j])
+						isSame = false;
+				}
+			}
+
+			return isSame;
+		}
+
+		/// <summary>
+		/// Initialisation du dgv et du tableau
+		/// </summary>
+		private void InitialisationDgv()
+		{
+			//On crée une variable hasard
+			Random rnd = new Random();
+			//On ajoute les colonnes au dgv
+			for (int i = 0; i < largeur; i++)
+			{
+				//On ne met rien comme valeur
+				DgvJeu.Columns.Add(i.ToString(), " ");
+				//On met la taille de la colonne à 10
+				DgvJeu.Columns[i].Width = 10;
+			}
+
+			//On ajoute les lignes au dgv
+			for (int i = 0; i < hauteur; i++)
+			{
+				DgvJeu.Rows.Add();
+				//On met la taille de la ligne a 10
+				DgvJeu.Rows[i].Height = 10;
+			}
+
+			//On redimensionne le dgv 
+			DgvJeu.Height = (hauteur + 1) * 10;
+			DgvJeu.Width = (largeur + 1) * 10;
+
+			//On initialise le tableau IsAlive
+			tabIsAlive = new int[largeur, hauteur];
+
+			//On met toutes les valeurs du tableau a -1 par defaut
+			for (int i = 0; i < largeur; i++)
+			{
+				for (int j = 0; j < hauteur; j++)
+				{
+					tabIsAlive[i, j] = -1;
+				}
+			}
+		}
+		
+		/// <summary>
+		/// Genere un tableau avec des valeurs positives et négatives
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btn_Random_Click(object sender, EventArgs e)
+		{
+			nbGeneration = 0;
+			lbl_Generation.Text = "Génération n° " + nbGeneration;
+
+			//On crée une variable hasard
+			Random rnd = new Random();
+			//On choisi le nombre de cellules qui seront vivantes (minimum 10 et maximum le nombre de cellules moins 5
+			int nbAlive = rnd.Next(10, (hauteur * largeur) - 5);
+			//Represente le numero de la ligne
+			int cellY = 0;
+			//Represente le numero de la colonne
+			int cellX = 0;
+
+			//On réinitialise le tableau isAlive
+			for (int i = 0; i < largeur; i++)
+			{
+				for(int j = 0; j < hauteur; j++)
+				{
+					tabIsAlive[i, j] = -1;
+				}
+			}
+
+			//On repete la boucle autant de fois que le nombre au hasard choisis pour le nombre de cellules vivantes à mettre
+			for (int i = 0; i < nbAlive; i++)
+			{
+				//On attribue des valeurs au hasard au variable representant le numero de la colonne et de la ligne
+				cellX = rnd.Next(0,largeur);
+				cellY = rnd.Next(0,hauteur);
+
+				//On met la cellule vivante
+				tabIsAlive[cellX,cellY] = 1;
+			}
+
+			//On parcours le dgv pour mettre les couleurs selon la valeur de la cellule
+			for (int i = 0; i < largeur; i++)
+			{
+				for (int j = 0; j < hauteur; j++)
+				{
+					//Si la cellule est vivante on la met noire
+					if (tabIsAlive[i,j] > 0)
+						DgvJeu.Rows[j].Cells[i].Style.BackColor = Color.Black;
+					//Si la cellule est morte on la met blanche
+					else
+						DgvJeu.Rows[j].Cells[i].Style.BackColor = Color.White;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Methode quand la tache de fond a fini de s'executé
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void bw_RunWorkerCompleted(Object sender, RunWorkerCompletedEventArgs e)
+		{
+			btn_Pause.Enabled = false;
+			btn_Play.Enabled = true;
+		}
+			
+		/// <summary>
+		/// Methode qui permet de passer de tabIsAlive(n) à tabIsAlive(n+1)
+		/// </summary>
+		public void NextStep()
+		{			
+			int nbCelluleVivante = 0;
+			int[,] tabTmp = new int[largeur, hauteur];
+
+			//On initialise le tableau tempon a -1
+			for (int i = 0; i < largeur; i++)
+			{
+				for (int j = 0; j < hauteur; j++)
+				{
+					tabTmp[i, j] = -1;
+				}
+			}
+
+			//On parcours le tableau IsAlive
+			for (int x = 0; x < largeur; x++)
+			{
+				for (int y = 0; y < hauteur; y++)
+				{
+					//On initialise le nombre de cellules vivantes autour de la cellule actuelle à 0
+					nbCelluleVivante = 0;
+
+					// Si on ne se trouve pas sur les bords
+					if (x > 0 && y > 0 && x < largeur - 1 && y < hauteur - 1)
+					{
+						//A chaque fois qu'une cellule voisine est vivante on incrémente la variable nbCelluleVivante
+						if (tabIsAlive[x - 1, y + 1] > 0)
+							nbCelluleVivante++;
+
+						if (tabIsAlive[x, y + 1] > 0)
+							nbCelluleVivante++;
+
+						if (tabIsAlive[x + 1, y + 1] > 0)
+							nbCelluleVivante++;
+
+						if (tabIsAlive[x - 1, y] > 0)
+							nbCelluleVivante++;
+
+						if (tabIsAlive[x + 1, y] > 0)
+							nbCelluleVivante++;
+
+						if (tabIsAlive[x - 1, y - 1] > 0)
+							nbCelluleVivante++;
+
+						if (tabIsAlive[x, y - 1] > 0)
+							nbCelluleVivante++;
+
+						if (tabIsAlive[x + 1, y - 1] > 0)
+							nbCelluleVivante++;
+
+						//On modifie dans un tableau tempon 
+						//Si le nb de cellules voisines vivantes est < 2 alors la cellule meurt
+						if (nbCelluleVivante < 2)
+							tabTmp[x, y] = -1;
+						//Si le nb de cellules voisines vivantes est > 3 alors la cellule meurt
+						else if (nbCelluleVivante > 3)
+							tabTmp[x, y] = -1;
+						//Si le nb de cellules voisines vivantes est = 3 alors la cellule nait
+						else if (nbCelluleVivante == 3)
+							tabTmp[x, y] = 1;
+						//Si le nb de cellules voisines vivantes est = 2 alors la cellule reste la meme
+						else if (nbCelluleVivante == 2)
+							tabTmp[x, y] = tabIsAlive[x, y];
+					}
+				}
+			}
+
+			lstIsAliveSauv.Add(tabIsAlive);
+			//On remplace l'ancien tableau isAlive par le tableau Tempon, passage de tabIsAlive(n) à tabIsAlive(n+1)
+			tabIsAlive = tabTmp;
+
+			//On actualise les couleurs du dgv
+			for (int i = 0; i < largeur; i++)
+			{
+				for (int j = 0; j < hauteur; j++)
+				{
+					if (tabIsAlive[i, j] > 0)
+						DgvJeu.Rows[j].Cells[i].Style.BackColor = Color.Black;
+					else
+						DgvJeu.Rows[j].Cells[i].Style.BackColor = Color.White;
+				}
+			}
+
+			nbGeneration++;
+		}
+
+		/// <summary>
+		/// Definie le nb de cellules
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Btn_Taille_Click(object sender, EventArgs e)
+		{
+			//On remet le dgv à 0
+			DgvJeu.Rows.Clear();
+			DgvJeu.Columns.Clear();
+
+			//Si la valeur pour la hauteur entrée par l'utilisateur est entre 15 et 60 on accepte la valeur
+			if (Convert.ToInt16(Num_Hauteur.Value) >= 15 && Convert.ToInt16(Num_Hauteur.Value) <= 60)
+			{
+				hauteur = Convert.ToInt16(Num_Hauteur.Value);
+				lbl_Erreur_Hauteur.ForeColor = Color.Black;
+			}			
+			else
+				lbl_Erreur_Hauteur.ForeColor = Color.Red;
+
+			//Si la valeur pour la largeur entrée par l'utilisateur est entre 15 et 60 on accepte la valeur
+			if (Convert.ToInt16(Num_Largeur.Value) >= 15 && Convert.ToInt16(Num_Largeur.Value) <= 80)
+			{
+				largeur = Convert.ToInt16(Num_Largeur.Value);
+				lbl_Erreur_Largeur.ForeColor = Color.Black;
+			}
+			else
+				lbl_Erreur_Largeur.ForeColor = Color.Red;
+				
+			//On initialise le dgv
+			InitialisationDgv();
+		}
+
+		/// <summary>
+		/// Methode quand on clique sur le bouton play
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btn_Play_Click(object sender, EventArgs e)
+		{
+			//On ne permet plus a l'utilisateur de modifier le dgv
+			DgvJeu.ClearSelection();
+			DgvJeu.Enabled = false;			
+			btn_Play.Enabled = false;
+
+			//L'utilisateur peut appuyer sur Pause
+			btn_Pause.Enabled = true;
+
+			//On lance la tache de fond
+			bw.RunWorkerAsync(sender);
+			
+		}
+		/// <summary>
+		/// Methode quand on clique sur le bouton pause 
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btn_Pause_Click(object sender, EventArgs e)
+		{			
+			if (bw.WorkerSupportsCancellation == true)
+			{
+				DgvJeu.Enabled = true;
+				// Annule l'opération asynchrone
+				bw.CancelAsync();
+
+				//L'utilisateur peut appuyer sur Play
+				btn_Play.Enabled = true;
+				btn_Pause.Enabled = false;
+				btn_Precedent.Enabled = true;
+
+				
+			}
+		}
+
+		/// <summary>
+		/// Methode quand l'utilisateur appuie sur le bouton Suivant
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btn_Suivant_Click(object sender, EventArgs e)
+		{
+			//On appelle la méthode qui amène le tabIsAlive(n) au tabIsAlive(n+1)
+			NextStep();
+			btn_Precedent.Enabled = true;
+
+			lbl_Generation.Text = "Génération n° " + nbGeneration;
+		}
+
+		/// <summary>
+		/// Permet de revenir à la génération précédente
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btn_Precedent_Click(object sender, EventArgs e)
+		{
+			int[,] tabTmp;
+			tabTmp = lstIsAliveSauv.ElementAt(lstIsAliveSauv.Count - 1);
+			//On actualise les couleurs du dgv
+			for (int i = 0; i < largeur; i++)
+			{
+				for (int j = 0; j < hauteur; j++)
+				{
+					if (tabTmp[i,j] > 0)
+						DgvJeu.Rows[j].Cells[i].Style.BackColor = Color.Black;
+					else
+						DgvJeu.Rows[j].Cells[i].Style.BackColor = Color.White;
+				}
+			}
+
+			lstIsAliveSauv.RemoveAt(lstIsAliveSauv.Count - 1);
+
+			if (lstIsAliveSauv.Count == 0)
+				btn_Precedent.Enabled = false;
+
+			tabIsAlive = tabTmp;
+			nbGeneration--;
+			lbl_Generation.Text = "Génération n° " + nbGeneration;
+		}
+		
+	}
+}
